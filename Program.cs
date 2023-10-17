259fe2b5cd53690804e2d7cf5b4278ec3ab261bf
Dell-User <Dell-User@DESKTOP-M748B2N>
2023-10-17 06:11:09 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Number_closest_to_100
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(check(78, 95));
+            Console.WriteLine(check(95,95));
+            Console.WriteLine(check(99,70));
+            Console.ReadKey();
+        }
+        public static int check(int x, int y)
+        {
+            const int n = 100;
+            var val1 = Math.Abs(x - n);
+            var val2 = Math.Abs(y - n);
+
+            return val1 == val2?0 : (val1<val2 ? x:y);
+
+        }
+    }
+}
